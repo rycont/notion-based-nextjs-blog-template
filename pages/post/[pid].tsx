4f15b2c027e1e1c347b058d5046d4bbe03a07893
rycont <reactdev@kakao.com>
2021-01-09 23:05:09 +0900

    fix : hide forbidden post

--- pages/post/[pid].tsx
+++ pages/post/[pid].tsx
@@ -2,20 +2,46 @@ import { getArticleWithBriefId } from 'notion-to-markdown'
 import Markdown from 'react-markdown'
 import { DATABASE_ID } from '../../constant'
 
-export default function Post({article}) {
+const Post: React.FC<{
+    article?: {
+        content: string;
+        pageId: string;
+        title: string;
+        tags: string[];
+    };
+    error?: string;
+}> = ({ article, error }) => {
+    if(error) {
+        if(process.browser) {
+            window.alert(error)
+            history.back()
+        }
+        return <></>
+    }
     return <div>
         <h1>{article.title}</h1>
+        <p>Tag : {article.tags.join(', ')}</p>
         <Markdown>{article.content}</Markdown>
     </div>
 }
 
+export default Post
+
 export async function getServerSideProps(context) {
-    const fetched = await getArticleWithBriefId(DATABASE_ID, context.query.pid)
-    console.log(fetched.content)
-    if(fetched)
+    try {
+        const fetched = await getArticleWithBriefId(DATABASE_ID, context.query.pid)
+        console.log(fetched.content)
+        if(fetched)
+            return {
+                props: {
+                    article: fetched
+                }
+            }
+    } catch {
         return {
             props: {
-                article: fetched
+                error: "Forbidden"
             }
         }
+    }
 }
